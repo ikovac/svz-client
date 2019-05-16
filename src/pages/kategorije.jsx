0e6350b089f406b9489bab5ab73b11906d6e835d
Ivo <ikovacevic96k@gmail.com>
2019-05-16 11:54:24 +0200

    started adding page kategorije

---
+++ src/pages/kategorije.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import Container from "../components/Container";
+import { graphql } from "gatsby";
+
+const KategorijePage = ({ data }) => {
+  return (
+    <Container>
+      {data.allNodeKategorije.edges.map(({ node }) => (
+        <p key={node.drupal_internal__nid}>{node.title}</p>
+      ))}
+    </Container>
+  );
+};
+
+export default KategorijePage;
+
+export const query = graphql`
+  query KategorijePageQuery {
+    allNodeKategorije(filter: { status: { eq: true } }) {
+      edges {
+        node {
+          title
+          drupal_internal__nid
+          path {
+            alias
+          }
+          relationships {
+            field_image {
+              localFile {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+            field_podkategorije {
+              field_link_kategorije {
+                uri
+                title
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
