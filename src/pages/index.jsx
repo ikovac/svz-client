4b503b5a64f4e8e2522784deead3d2d5af2933c1
ikovac <ikovacevic96k@gmail.com>
2019-05-26 19:09:22 +0200

    added basic elements on front page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,10 +4,22 @@ import SEO from "../components/seo";
 
 import "../styles/main.scss";
 import Timeline from "../components/Timeline";
+import Image from "../components/Image";
 
 const IndexPage = ({ data }) => (
   <div className="front-page-wrapper">
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <div className="front-page__image-section">
+      <Image
+        source={data.placeholderImage.childImageSharp.fluid}
+        alt={"Sve za vjenčanje"}
+      />
+      <h2>Sve za Vjenčanje</h2>
+      <h3>Isplanirajte Vaše vjenčanje u samo par koraka</h3>
+      <div className="front-page__form">
+        <input type="date"/>
+      </div>
+    </div>
     <Timeline />
   </div>
 );
@@ -18,7 +30,7 @@ export const query = graphql`
   query frontPageImage {
     placeholderImage: file(relativePath: { eq: "cover.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 2000) {
+        fluid(maxWidth: 3000, maxHeight: 700) {
           ...GatsbyImageSharpFluid
         }
       }
