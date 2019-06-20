182254817ebd8ae6d90cf8da3945ca0688c522ce
ikovac <ikovacevic96k@gmail.com>
2019-06-20 19:43:08 +0200

    added some styles to article-teaser page

--- src/components/UslugeFilters/index.jsx
+++ src/components/UslugeFilters/index.jsx
@@ -8,6 +8,8 @@ import { StaticQuery, graphql } from "gatsby";
 import DatumSelect from "./DatumSelect";
 import KapacitetInput from "./KapacitetInput";
 
+import { FaTimes } from "react-icons/fa";
+
 class UslugeFilters extends Component {
   constructor(props) {
     super(props);
@@ -18,43 +20,45 @@ class UslugeFilters extends Component {
       kapacitet: filtersStore.kapacitet,
       razglas: null,
       displayDate: filtersStore.datum ? new Date(filtersStore.datum) : null,
+      showFilters: false,
     };
     this.initialSubmit();
   }
 
   handleInputChange = e => {
-    const {updateFilters} = this.props;
+    const { updateFilters } = this.props;
     this.setState({ kapacitet: e.target.value });
-    updateFilters('kapacitet', e.target.value);    
+    updateFilters("kapacitet", e.target.value);
   };
 
   handleDatumChange = date => {
-    const {updateFilters} = this.props;
+    const { updateFilters } = this.props;
     if (date) {
       let datum = `${date.getFullYear()}-${date.getMonth() +
         1}-${date.getDate()}`;
-        this.setState({ datum: datum, displayDate: date });
-        updateFilters('datum', datum);
-      } else {
-        this.setState({ datum: null, displayDate: date });
-        updateFilters('datum', null);
+      this.setState({ datum: datum, displayDate: date });
+      updateFilters("datum", datum);
+    } else {
+      this.setState({ datum: null, displayDate: date });
+      updateFilters("datum", null);
     }
   };
 
   handleLokacijaChange = name => {
-    const {updateFilters} = this.props;
+    const { updateFilters } = this.props;
     this.setState({ lokacija: name });
-    updateFilters('lokacija', name);
+    updateFilters("lokacija", name);
   };
 
   handleFiltersSubmit = e => {
     e.preventDefault();
     this.props.onFilterSubmit(this.state);
+    this.onFilterClose();
   };
 
   initialSubmit = () => {
     this.props.onFilterSubmit(this.state);
-  }
+  };
 
   handleKeyDown = e => {
     if (e.keyCode === 13) {
@@ -66,8 +70,30 @@ class UslugeFilters extends Component {
     e.preventDefault();
     if (e.keyCode === 13) {
       this.props.onFilterSubmit(this.state);
+      this.onFilterClose();
     }
   };
+
+  onFilterToggle = e => {
+    e.preventDefault();
+    const {showFilters} = this.state;
+
+    if(showFilters) {
+      this.onFilterClose();
+    } else {
+      this.onFilterOpen();
+    }
+  };
+
+  onFilterOpen = () => {
+    this.setState({ showFilters: true });
+    document.getElementsByTagName("body")[0].style.position = "fixed";
+  }
+
+  onFilterClose = () => {
+    this.setState({ showFilters: false });
+    document.getElementsByTagName("body")[0].style.position = "unset";
+  }
 
   componentDidMount() {
     document
@@ -77,7 +103,7 @@ class UslugeFilters extends Component {
 
   render() {
     const { filters } = this.props;
-    const { displayDate, lokacija, kapacitet } = this.state;
+    const { displayDate, lokacija, kapacitet, showFilters } = this.state;
 
     return (
       <StaticQuery
@@ -99,8 +125,23 @@ class UslugeFilters extends Component {
         `}
         render={lokacijaTerms => (
           <div className="filters-container">
+            <button
+              className="button filter-btn"
+              onClick={this.onFilterToggle}
+            >
+              Filteri
+            </button>
             {/* <h4>Filtriraj rezultate</h4> */}
-            <form onKeyDown={this.handleKeyDown} autoComplete="lalala">
+            <form
+              onKeyDown={this.handleKeyDown}
+              autoComplete="lalala"
+              className={showFilters ? null : "close"}
+            >
+              <div className="form-filters-controls">
+                <button onClick={this.onFilterToggle}>
+                  <FaTimes />
+                </button>
+              </div>
               {filters.includes("datum") && (
                 <DatumSelect
                   displayDate={displayDate}
