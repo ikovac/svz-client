dd306b59fcfc92b86aa930dcc9818ed4967100c5
ikovac <ikovacevic96k@gmail.com>
2019-06-25 19:12:21 +0200

    removed redux-logger for production test

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import rootReducer from "../reducers/rootReducer";
 
-const middleware = [thunk, logger];
+// const middleware = [thunk, logger];
+const middleware = [thunk];
 
 const store = createStore(rootReducer, applyMiddleware(...middleware));
 
