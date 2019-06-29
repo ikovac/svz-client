6b822311f38658511d1189a3e9c66ccf033c45ba
ikovac <ikovacevic96k@gmail.com>
2019-06-29 11:02:02 +0200

    first commit

--- src/utils/trimHtmlText.js
+++ src/utils/trimHtmlText.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-export default function getTextSummary(html, length = 300) {
-  let text = html.replace(/<[^>]*>/g, "");
-  text = text.replace(/&nbsp;/g, " ");
-  return <p>{text.substring(0, length).trim() + "..."}</p>;
+export default function getTextSummary(body, size = 300) {
+  let bodyLength = body.length;
+
+  if(!bodyLength) {
+    return body;
+  }
+  if(!size) {
+    return body;
+  }
+
+  if(bodyLength <= size) {
+    return body;
+  }
 }
